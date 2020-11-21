02c05dd627aae1da700099c6f3619c87e708de2f
MichelleRobson <71073643+MichelleRobson@users.noreply.github.com>
2020-11-21 07:50:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var ball,database,position;
+
+function setup() {
+    database=firebase.database();
+    console.log(database);
+    createCanvas(500,500);
+
+    ball=createSprite(250,250,30,30);
+    ball.shapeColor="blue";
+
+    var ballPosition=database.ref('ball/position');
+    ballPosition.on("value",readPosition,showError);
+}
+
+function draw() {
+    background(255);
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,1);
+    }
+    drawSprites();
+}
+
+function readPosition(data) {
+    position=data.val();
+    console.log(position.x);
+    ball.x= position.x;
+    ball.y= position.y;
+}
+function writePosition(x,y){
+    database.ref('ball/position').set({ 
+       'x':position.x+x,
+        'y': position.y+y
+    })
+}
